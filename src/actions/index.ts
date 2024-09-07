fc6474478650ed9ad17cbfce926ca948629409ff
raghav369r <raghav1010reddy@gmail.com>
2024-09-07 16:54:35 +0530

    added server actions for signIn and signOut

---
+++ src/actions/index.ts
@@ -0,0 +1,10 @@
+"use server";
+import * as auth from "@/auth";
+
+export const signIn = () => {
+  // will get provider as parameter
+  return auth.signIn("github");
+};
+export const signOut = () => {
+  return auth.signOut();
+};
